203e7a30eda79f782186594e083ab48861a9d5e8
ennuiendo <124878728+ennuiendo@users.noreply.github.com>
2024-12-22 19:34:29 +0000

    Add optional params to line arrivals

--- line.go
+++ line.go
@@ -22,38 +22,14 @@ func (c *Client) GetLineStatusByMode(ctx context.Context, modes []string) ([]Sta
 	return statuses, err
 }
 
-package tfl
-
-import (
-	"context"
-	"fmt"
-
-	"github.com/jamesalexatkin/tfl-golang/internal/conv"
-)
-
-// GetLineStatusByMode gets the line status of all lines for the given modes.
-//
-// https://api.tfl.gov.uk/swagger/ui/index.html?url=/swagger/docs/v1#!/Line/Line_StatusByMode
-func (c *Client) GetLineStatusByMode(ctx context.Context, modes []string) ([]Status, error) {
-	path := fmt.Sprintf("/Line/Mode/%s/Status", conv.StringSliceToString(modes))
-
-	statuses := []Status{}
-	err := c.get(ctx, path, &statuses)
-	if err != nil {
-		return nil, err
-	}
-
-	return statuses, err
-}
-
 // GetLineArrivals gets the list of arrival predictions for given line ids based at the given stop.
 //
 // https://api.tfl.gov.uk/swagger/ui/index.html?url=/swagger/docs/v1#!/Line/Line_Arrivals
-func (c *Client) GetLineArrivals(ctx context.Context, lines []string, stopid string) ([]Prediction, error) {
+func (c *Client) GetLineArrivals(ctx context.Context, lines []string, stopid string, params map[string]string) ([]Prediction, error) {
 	path := fmt.Sprintf("/Line/%s/Arrivals/%s", conv.StringSliceToString(lines), stopid)
 
 	predictions := []Prediction{}
-	err := c.get(ctx, path, &predictions)
+	err := c.getWithQueryParams(ctx, path, params, &predictions)
 	if err != nil {
 		return nil, err
 	}
